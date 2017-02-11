3ee21634dabe9b4bf5ffc41844aebf9c4381c0ad
Daniel Alhadeff <dalhadeff@olin.edu>
2017-02-11 15:15:44 -0500

    added music

---
+++ unity/Assets/Music/Music_Control.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Music_Control : MonoBehaviour {
+	
+	AudioSource audio;
+	public float emoRate;
+
+	void Update() {
+		audio = GetComponent<AudioSource>();
+		audio.pitch = Mathf.Clamp(1f + emoRate*SampleApp.emo, -3f, 3f);
+	}
+
+
+}
